Fix the std. dev to 1 to avoid gradient issues
When using a Gaussian, if the std. dev becomes close to zero
(the gaussian collapses on a single point) the gradients of
the encoder become too big compared to the G and D models.

Additionally, reduce the beta_1 momentum coeff. in Adam.

learn/stats/distributions.py
@@ -66,11 +66,17 @@ class IsotropicGaussian(Distribution):
         sample = mean + std * eps
         return sample
 
-    def nll(self, samples, param_dict):
+    def nll(self, samples, param_dict, use_std=False):
         mean = param_dict['mean']
-        std = param_dict['std']
+        # using the std. dev should be done with caution, as it can
+        # result in very large gradients in the beginning of training
+        if use_std:
+            std = param_dict['std']
+        else:
+            std = 1.0
+
         return K.sum(
-            0.5 * np.log(2 * np.pi + K.epsilon()) + K.log(std + K.epsilon()) +
+            0.5 * np.log(2 * np.pi) + K.log(std + K.epsilon()) +
             0.5 * K.square((samples - mean) / (std + K.epsilon())),
             axis=-1)
 